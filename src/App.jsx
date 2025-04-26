312b5167fa2bee19d8e1abdf87adb77c75582444
Emir <itsmohebbi@gmail.com>
2025-04-26 16:33:19 +0330

    second version of the app

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import ToDo from "./components/ToDo";
 function App() {
   return (
     <>
-    <ToDo />
+      <ToDo />
     </>
   );
 }
--- src/components/ToDo.jsx
+++ src/components/ToDo.jsx
@@ -1,17 +1,33 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import styles from "./ToDo.module.css";
 function ToDo() {
-  const [tasks, setTasks] = useState(["wash the dishess", "do your homework"]);
+  // give the tasks form local storage
+  const [tasks, setTasks] = useState(() => {
+    const saved = localStorage.getItem("tasks");
+    return saved ? JSON.parse(saved) : [];
+  });
+  // a useState for each new task
   const [newTask, setNewTask] = useState("");
 
+  // every time the tasks array changed save it in local storage
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }, [tasks]);
+
+  // add new task func
   const addTask = () => {
-    if (newTask) {
+    // if the newTask is not empty and without any spaces add the task
+    if (newTask && newTask.trim()) {
       setTasks((tasks) => [...tasks, newTask]);
+      setNewTask("");
     }
   };
+
+  // func for delete task
   const deleteHandeler = (index) => {
     setTasks(tasks.filter((task, i) => index !== i));
   };
+  // func for moving up a task
   const moveUp = (index) => {
     if (index > 0) {
       const updatedTasks = [...tasks];
@@ -22,6 +38,7 @@ function ToDo() {
       setTasks(updatedTasks);
     }
   };
+  // func for moving down a task
   const moveDown = (index) => {
     if (index < tasks.length - 1) {
       const updatedTasks = [...tasks];
@@ -32,6 +49,12 @@ function ToDo() {
       setTasks(updatedTasks);
     }
   };
+  // when user click the Enter button add task
+  const keyDown = (e) => {
+    if (e.key === "Enter") {
+      addTask();
+    }
+  };
   return (
     <div className={styles.container}>
       <h1>Todo App</h1>
@@ -39,6 +62,7 @@ function ToDo() {
         type="text"
         placeholder="Enter New Task ..."
         value={newTask}
+        onKeyDown={keyDown}
         onChange={(e) => setNewTask(e.target.value)}
       />
       <button className={styles.add} onClick={addTask}>
